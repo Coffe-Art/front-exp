71d747dfd48ae0a052ba86d5096891fcf2cd77a4
Coffe-Art <CoffeArt224@gmail.com>
2024-08-02 16:09:14 -0500

    Add files via upload

--- src/components/Layouts/Companies.jsx
+++ src/components/Layouts/Companies.jsx
@@ -1,51 +1,59 @@
-import React, { useState } from 'react';
+// src/components/Layouts/Companies.jsx
+import React, { useEffect, useState } from 'react';
+import { useEmpresa } from '../../Context/contextEmpresa';
 import { Header } from './Header';
+import { NavLink } from 'react-router-dom';
 import { Footer } from './Footer';
 import { useNavigate } from 'react-router-dom';
 
 export const Companies = () => {
-    const [empresas, setEmpresas] = useState([
-        { id: 1, nombre: 'Empresa A', email: 'empresaA@example.com', telefono: '123-456-7890', rol: 'admin', descripcion: 'Descripción de la Empresa A' },
-        { id: 2, nombre: 'Empresa B', email: 'empresaB@example.com', telefono: '987-654-3210', rol: 'seller', descripcion: 'Descripción de la Empresa B' },
-    ]);
-
-    const [showForm, setShowForm] = useState(false);
-    const [nombre, setNombre] = useState('');
-    const [email, setEmail] = useState('');
-    const [telefono, setTelefono] = useState('');
-    const [rol, setRol] = useState('admin');
-    const [descripcion, setDescripcion] = useState('');
-
+    const { empresas, setEmpresas } = useEmpresa();
     const navigate = useNavigate();
+    const [userId, setUserId] = useState(null);
 
-    // Toggle form visibility
-    const toggleForm = () => {
-        setShowForm(prev => !prev);
+    useEffect(() => {
+        const storedUserId = localStorage.getItem('userId');
+        if (storedUserId) {
+            console.log('User ID encontrado en localStorage:', storedUserId);
+            setUserId(storedUserId);
+            fetchEmpresas(storedUserId);
+        } else {
+            console.log('No se encontró User ID en localStorage');
+        }
+    }, []);
+
+    const fetchEmpresas = async (idadministrador) => {
+        try {
+            const response = await fetch(`https://backtesteo.onrender.com/api/empresa/consultarPorAdministrador/${idadministrador}`);
+            const contentType = response.headers.get("content-type");
+
+            console.log('Respuesta del servidor:', response);
+
+            if (!response.ok) {
+                throw new Error('Error al obtener empresas');
+            }
+
+            if (contentType && contentType.indexOf("application/json") !== -1) {
+                const data = await response.json();
+                console.log('Datos recibidos del servidor:', data);
+
+                if (Array.isArray(data) && Array.isArray(data[0])) {
+                    setEmpresas(data[0]);
+                } else {
+                    throw new Error('Formato de datos inesperado');
+                }
+            } else {
+                const text = await response.text();
+                console.error('Respuesta no es JSON:', text);
+                throw new Error('Respuesta no es JSON');
+            }
+        } catch (error) {
+            console.error('Error al obtener empresas:', error);
+        }
     };
 
-    // Add a new company
-    const handleAddEmpresa = (e) => {
-        e.preventDefault();
-        const newEmpresa = {
-            id: empresas.length + 1,
-            nombre,
-            email,
-            telefono,
-            rol,
-            descripcion,
-        };
-        setEmpresas([...empresas, newEmpresa]);
-        setNombre('');
-        setEmail('');
-        setTelefono('');
-        setRol('admin');
-        setDescripcion('');
-        setShowForm(false); // Hide the form after submission
-    };
-
-    // Navigate to company detail view
     const viewEmpresa = (id) => {
-        navigate(`/company/${id}`); 
+        navigate(`/company/${id}`);
     };
 
     return (
@@ -53,110 +61,49 @@ export const Companies = () => {
             <div className="flex flex-col min-h-screen bg-gray-200">
                 <Header />
                 <div className="container mx-auto my-8 grid gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
-                    {empresas.map(empresa => (
-                        <div
-                            key={empresa.id}
-                            className="bg-white border rounded-lg overflow-hidden shadow-md cursor-pointer flex flex-col items-center p-4"
-                            onClick={() => viewEmpresa(empresa.id)}
-                        >
-                            <h3 className="text-lg font-semibold mb-2 text-center">{empresa.nombre}</h3>
-                            <p className="text-center">{empresa.descripcion}</p>
+                    {empresas.length === 0 ? (
+                        <div className="bg-white border rounded-lg overflow-hidden shadow-md flex flex-col items-center p-4 cursor-pointer">
+                            <div className="flex flex-col items-center">
+                                <span className="text-black text-sm text-center">No hay empresas para mostrar</span>
+                            </div>
                         </div>
-                    ))}
-                    <div
-                        className="bg-white border rounded-lg overflow-hidden shadow-md flex flex-col items-center p-4 cursor-pointer"
-                        onClick={toggleForm}
-                    >
-                        <svg
-                            className="w-8 h-8 mb-2 text-darkyellow"
-                            fill="none"
-                            stroke="currentColor"
-                            viewBox="0 0 24 24"
-                            xmlns="http://www.w3.org/2000/svg"
-                        >
-                            <path
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                strokeWidth="2"
-                                d="M12 4v16m8-8H4"
-                            ></path>
-                        </svg>
-                        <span className="text-black text-sm text-center">Crear Evento</span>
+                    ) : (
+                        empresas.map(empresa => (
+                            <div
+                                key={empresa.id}
+                                className="bg-white border rounded-lg overflow-hidden shadow-md cursor-pointer flex flex-col items-center p-4"
+                                onClick={() => viewEmpresa(empresa.id)}
+                            >
+                                <h3 className="text-lg font-semibold mb-2 text-center">{empresa.nombre}</h3>
+                                <p className="text-center underline text-darkyellow">{empresa.direccion}</p>
+                                <p className="text-center line-clamp-5">{empresa.descripcion}</p>
+                                <p className="text-center text-gray-600">ID: {empresa.codigoempresa}</p>
+                            </div>
+                        ))
+                    )}
+                    <div className="bg-white border rounded-lg overflow-hidden shadow-md flex flex-col items-center p-4 cursor-pointer">
+                        <div className="flex flex-col items-center" onClick={() => navigate('/LoginCompanies')}>
+                            <svg
+                                className="w-8 h-8 mb-2 text-darkyellow"
+                                fill="none"
+                                stroke="currentColor"
+                                viewBox="0 0 24 24"
+                                xmlns="http://www.w3.org/2000/svg"
+                            >
+                                <path
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    strokeWidth="2"
+                                    d="M12 4v16m8-8H4"
+                                ></path>
+                            </svg>
+                            <span className="text-black text-sm text-center">Agregar una empresa nueva</span>
+                            <NavLink to="/LoginCompanies" className="text-darkyellow hover:underline text-sm ml-2">
+                                Registrar Empresa
+                            </NavLink>
+                        </div>
                     </div>
                 </div>
-
-                {/* Conditionally render the form */}
-                {showForm && (
-                    <div className="fixed inset-0 flex items-center justify-center bg-gray-900 bg-opacity-50 z-50">
-                        <div className="bg-white p-8 rounded-lg shadow-lg max-w-sm w-full">
-                            <h2 className="text-xl font-semibold mb-4">Agregar Empresa</h2>
-                            <form onSubmit={handleAddEmpresa}>
-                                <div className="mb-4">
-                                    <label className="block text-gray-700">Nombre:</label>
-                                    <input
-                                        type="text"
-                                        value={nombre}
-                                        onChange={(e) => setNombre(e.target.value)}
-                                        className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
-                                    />
-                                </div>
-                                <div className="mb-4">
-                                    <label className="block text-gray-700">Email:</label>
-                                    <input
-                                        type="email"
-                                        value={email}
-                                        onChange={(e) => setEmail(e.target.value)}
-                                        className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
-                                    />
-                                </div>
-                                <div className="mb-4">
-                                    <label className="block text-gray-700">Teléfono:</label>
-                                    <input
-                                        type="text"
-                                        value={telefono}
-                                        onChange={(e) => setTelefono(e.target.value)}
-                                        className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
-                                    />
-                                </div>
-                                <div className="mb-4">
-                                    <label className="block text-gray-700">Rol:</label>
-                                    <select
-                                        value={rol}
-                                        onChange={(e) => setRol(e.target.value)}
-                                        className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
-                                    >
-                                        <option value="admin">Admin</option>
-                                        <option value="seller">Seller</option>
-                                    </select>
-                                </div>
-                                <div className="mb-4">
-                                    <label className="block text-gray-700">Descripción:</label>
-                                    <textarea
-                                        value={descripcion}
-                                        onChange={(e) => setDescripcion(e.target.value)}
-                                        rows="4"
-                                        className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
-                                    ></textarea>
-                                </div>
-                                <div className="flex justify-end">
-                                    <button
-                                        type="submit"
-                                        className="bg-indigo-600 text-white px-4 py-2 rounded-md shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500"
-                                    >
-                                        Agregar
-                                    </button>
-                                    <button
-                                        type="button"
-                                        onClick={toggleForm}
-                                        className="ml-4 bg-gray-600 text-white px-4 py-2 rounded-md shadow-sm hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-gray-500"
-                                    >
-                                        Cancelar
-                                    </button>
-                                </div>
-                            </form>
-                        </div>
-                    </div>
-                )}
             </div>
             <Footer />
         </div>
--- src/components/Layouts/LoginCompanies.jsx
+++ src/components/Layouts/LoginCompanies.jsx
@@ -1,10 +1,70 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
-import BackgroundImage from '../../assets/BackgroundLogin.jpg'; 
+import React, { useState, useEffect } from 'react';
+import { NavLink, useNavigate } from 'react-router-dom';
+import BackgroundImage from '../../assets/BackgroundLogin.jpg';
 import { FaHome } from 'react-icons/fa';
 import Logo from '../../assets/Artesanías.png';
+import { useEmpresa } from '../../Context/contextEmpresa'; // Importa el hook para usar el contexto
 
 export const LoginCompanies = () => {
+    const [nombre, setNombre] = useState('');
+    const [direccion, setDireccion] = useState('');
+    const [descripcion, setDescripcion] = useState('');
+    const [idadministrador, setIdAdministrador] = useState('');
+    
+    const navigate = useNavigate();
+    const { setEmpresas } = useEmpresa(); // Obtén setEmpresas desde el contexto
+
+    useEffect(() => {
+        // Obtener el ID del administrador del localStorage
+        const storedId = localStorage.getItem('userId');
+        if (storedId) {
+            setIdAdministrador(storedId);
+        }
+    }, []);
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        const newEmpresa = {
+            nombre,
+            direccion,
+            descripcion,
+            idadministrador
+        };
+
+        try {
+            const response = await fetch('https://backtesteo.onrender.com/api/empresa/nuevaEmpresa', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(newEmpresa),
+            });
+
+            if (!response.ok) {
+                const errorDetails = await response.json();
+                console.error('Error en la creación de la empresa:', errorDetails);
+                throw new Error('Error en la creación de la empresa');
+            }
+
+            const result = await response.json();
+            console.log('Empresa creada exitosamente:', result);
+
+            // Obtén el ID de la nueva empresa
+            const empresaId = result.id; 
+
+            // Actualiza la lista de empresas en el contexto
+            setEmpresas(prevEmpresas => [
+                ...prevEmpresas,
+                { id: empresaId, nombre, direccion, descripcion }
+            ]);
+
+            // Redirige a la página de empresas
+            navigate('/companies');
+        } catch (error) {
+            console.error('Error al crear la empresa:', error);
+        }
+    };
 
     return (
         <div 
@@ -19,40 +79,49 @@ export const LoginCompanies = () => {
             <div className="relative bg-white p-5 rounded-lg shadow-md w-full max-w-md mx-4 sm:mx-8 md:mx-16 lg:mx-32">
                 <img src={Logo} alt="Logo" className="h-24 w-24 mx-auto mb-6" />
                 <h1 className="text-2xl font-bold mb-6 text-center">Registrar Empresa</h1>
-                <form>
+                <form onSubmit={handleSubmit}>
                     <div className="mb-4">
-                        <label className="block text-black text-sm font-bold mb-2" htmlFor="username">
+                        <label className="block text-black text-sm font-bold mb-2" htmlFor="nombre">
                             Nombre de Empresa
                         </label>
                         <input
                             type="text"
-                            id="username"
+                            id="nombre"
+                            value={nombre}
+                            onChange={(e) => setNombre(e.target.value)}
                             className="shadow appearance-none border rounded w-full py-2 px-3 text-darkyellow leading-tight focus:outline-none focus:shadow-outline"
                             placeholder="Nombre de Empresa"
+                            required
                         />
                     </div>
                     <div className="mb-4">
-                        <label className="block text-black text-sm font-bold mb-2" htmlFor="email">
+                        <label className="block text-black text-sm font-bold mb-2" htmlFor="direccion">
                             Dirección
                         </label>
                         <input
-                            type="email"
-                            id="email"
+                            type="text"
+                            id="direccion"
+                            value={direccion}
+                            onChange={(e) => setDireccion(e.target.value)}
                             className="shadow appearance-none border rounded w-full py-2 px-3 text-darkyellow leading-tight focus:outline-none focus:shadow-outline"
                             placeholder="Dirección"
+                            required
                         />
                     </div>
                     <div className="mb-4">
-                        <label className="block text-black text-sm font-bold mb-2" htmlFor="password">
+                        <label className="block text-black text-sm font-bold mb-2" htmlFor="descripcion">
                             Descripción
                         </label>
-                        <input
-                            type="password"
-                            id="password"
+                        <textarea
+                            id="descripcion"
+                            value={descripcion}
+                            onChange={(e) => setDescripcion(e.target.value)}
                             className="shadow appearance-none border rounded w-full py-2 px-3 text-darkyellow leading-tight focus:outline-none focus:shadow-outline"
                             placeholder="Descripción"
+                            required
                         />
                     </div>
+                    {/* El campo ID Administrador se ha eliminado del formulario */}
                     <div className="flex items-center justify-center">
                         <button
                             type="submit"
