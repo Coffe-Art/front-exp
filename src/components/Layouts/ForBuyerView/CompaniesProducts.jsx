c696a7ec2053f342ee91911ade04c748850113a5
Coffe Art 224 <CoffeArt224@gmail.com>
2024-09-08 20:20:35 -0500

    Update CompaniesProducts Vista -MQ

--- src/components/Layouts/ForBuyerView/CompaniesProducts.jsx
+++ src/components/Layouts/ForBuyerView/CompaniesProducts.jsx
@@ -1,90 +1,322 @@
-import React, { useEffect, useState } from 'react';
-import { useParams, useNavigate } from 'react-router-dom';
+import React, { useState, useEffect, useContext } from 'react';
+import { useNavigate, useParams } from 'react-router-dom'; // Agregar useParams para obtener parámetros de URL
+import { FaPlus, FaSearch, FaStar } from 'react-icons/fa';
 import { Header } from '../ForView/Header';
 import { Footer } from '../ForView/Footer';
-import { FaStar } from "react-icons/fa";
+import ProductoContext from '../../../Context/contextProducto';
+import CartIcon from './CartIcon';
 
 export const CompaniesProducts = () => {
-    const { codigoempresa } = useParams();
-    const [productos, setProductos] = useState([]);
-    const [loadingMessage, setLoadingMessage] = useState('');
-    const [error, setError] = useState('');
-    const navigate = useNavigate();
+  const { productos, setProductos } = useContext(ProductoContext);
+  const [productosFiltrados, setProductosFiltrados] = useState([]);
+  const [filtroAbierto, setFiltroAbierto] = useState(false);
+  const [terminoBusqueda, setTerminoBusqueda] = useState('');
+  const [categoria, setCategoria] = useState('todos');
+  const [precioMinimo, setPrecioMinimo] = useState(0);
+  const [precioMaximo, setPrecioMaximo] = useState(0);
+  const [calificacion, setCalificacion] = useState(0);
+  const [error, setError] = useState(null);
+  const [cargando, setCargando] = useState(true);
+  const [opcionesCategorias, setOpcionesCategorias] = useState([]);
+  const [productoSeleccionado, setProductoSeleccionado] = useState(null);
+  const [modalAbierto, setModalAbierto] = useState(false);
+  const [loadingMessage, setLoadingMessage] = useState(''); // Agregado para manejar el mensaje de carga
 
-    useEffect(() => {
-        if (codigoempresa) {
-            fetchProductos(codigoempresa);
+  const navigate = useNavigate();
+  const { codigoempresa } = useParams(); // Obtener el ID de la empresa desde los parámetros de la URL
+
+  const fetchProductos = async (codigoempresa) => {
+    setCargando(true); // Establece cargando en true al comenzar la solicitud
+    setLoadingMessage(`Obteniendo productos para la empresa con código: ${codigoempresa}`);
+    
+    try {
+      const response = await fetch(`https://checkpoint-9tp4.onrender.com/api/comprador/ver/empresa/${codigoempresa}`, {
+        headers: {
+          'Authorization': `Bearer ${localStorage.getItem('token')}`
         }
-    }, [codigoempresa]);
-    
-    const fetchProductos = async (codigoempresa) => {
-        try {
-            setLoadingMessage(`Obteniendo productos para la empresa con código: ${codigoempresa}`);
-            
-            const response = await fetch(`https://checkpoint-9tp4.onrender.com/api/comprador/ver/empresa/${codigoempresa}`);
-    
-            if (!response.ok) {
-                throw new Error('Error al obtener productos desde el servidor');
-            }
-    
-            const [productosData] = await response.json();
-    
-            if (Array.isArray(productosData)) {
-                setProductos(productosData);
-            } else {
-                throw new Error('Los datos de productos no son un array');
-            }
-    
-            setLoadingMessage('');
-        } catch (error) {
-            setError('Error al obtener productos');
-            setLoadingMessage('');
-            console.error('Error al obtener productos:', error.message);
-        }
+      });
+  
+      if (!response.ok) {
+        throw new Error('Error al obtener productos desde el servidor');
+      }
+  
+      const [productosData] = await response.json();
+  
+      if (Array.isArray(productosData)) {
+        setProductos(productosData);
+        console.log('Productos recibidos:', productosData);
+      } else {
+        throw new Error('Los datos de productos no son un array');
+      }
+    } catch (error) {
+      setError('Error al obtener productos');
+      console.error('Error al obtener productos:', error.message);
+    } finally {
+      setCargando(false); // Establece cargando en false después de la solicitud
+      setLoadingMessage('');
+    }
+  };
+  
+
+  useEffect(() => {
+    if (codigoempresa) {
+      fetchProductos(codigoempresa);
+    }
+  }, [codigoempresa]);
+
+  useEffect(() => {
+    const obtenerCategorias = () => {
+      const categorias = [
+        { value: 'joyeria', label: 'Joyería' },
+        { value: 'ropa_y_accesorios', label: 'Ropa y Accesorios' },
+        { value: 'ceramica', label: 'Cerámica' },
+        { value: 'muebles', label: 'Muebles' },
+        { value: 'decoracion', label: 'Decoración' },
+        { value: 'arte_textil', label: 'Arte Textil' },
+        { value: 'productos_de_madera', label: 'Productos de Madera' },
+        { value: 'cosmeticos_y_cuidado_personal', label: 'Cosméticos y Cuidado Personal' },
+        { value: 'papeleria_y_libros', label: 'Papelería y Libros' },
+        { value: 'articulos_para_el_hogar', label: 'Artículos para el Hogar' },
+        { value: 'juguetes_y_juegos', label: 'Juguetes y Juegos' },
+        { value: 'instrumentos_musicales', label: 'Instrumentos Musicales' },
+        { value: 'productos_ecologicos', label: 'Productos Ecológicos' },
+        { value: 'productos_para_mascotas', label: 'Productos para Mascotas' },
+        { value: 'otro', label: 'Otro' },
+      ];
+
+      setOpcionesCategorias(categorias);
     };
 
-    const handleGoHome = () => {
-        navigate('/CompaniesComprador');
-    };
+    obtenerCategorias();
+  }, []);
 
-    return (
-        <div className="flex flex-col min-h-screen bg-gray-200">
-            <Header />
-            <div className="flex-1 p-4">
-                {loadingMessage && <p className="text-center text-gray-600">{loadingMessage}</p>}
-                {error && <p className="text-center text-red-600">{error}</p>}
-                {!loadingMessage && !error && (
-                    <div className="grid gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
-                        {productos.map(product => (
-                            <div key={product.idProducto} className="bg-white border rounded-lg overflow-hidden shadow-md flex flex-col items-center p-4">
-                                <img
-                                    src={`https://imagenes224.blob.core.windows.net/imagenes224/${product.urlProductoImg.split('/').pop()}`}
-                                    alt={product.nombre}
-                                    className="w-full h-48 object-cover"
-                                />
-                                <h3 className="text-lg font-semibold mb-2 text-center">{product.nombre}</h3>
-                                <p className="text-center"><strong>Publicado por:</strong> {product.publicadoPor}</p>
-                                <p className="text-sm text-darkpurple mb-2 text-center">${product.precio}</p>
-                                <div className="flex items-center mb-2">
-                                    {[1, 2, 3, 4, 5].map(star => (
-                                        <FaStar
-                                            key={star}
-                                            className={`text-yellow-500 ${star <= product.rating ? 'text-yellow-500' : 'text-gray-300'}`}
-                                        />
-                                    ))}
-                                </div>
-                                <button
-                                    onClick={() => addToCart(product)}
-                                    className="bg-darkpurple text-white py-2 px-4 rounded hover:bg-purple-700"
-                                >
-                                    Añadir al Carrito
-                                </button>
-                            </div>
-                        ))}
-                    </div>
-                )}
+  useEffect(() => {
+    filtrarProductos(terminoBusqueda, categoria, [precioMinimo, precioMaximo], calificacion);
+  }, [terminoBusqueda, categoria, precioMinimo, precioMaximo, calificacion, productos]);
+
+  const filtrarProductos = (terminoBusqueda, categoria, [precioMinimo, precioMaximo], calificacion) => {
+    if (productos.length === 0) return;
+
+    const productosFiltrados = productos.filter(producto => {
+      if (!producto) return false;
+
+      const coincideBusqueda = (producto.nombre && producto.nombre.toLowerCase().includes(terminoBusqueda.toLowerCase())) ||
+                               (producto.descripcion && producto.descripcion.toLowerCase().includes(terminoBusqueda.toLowerCase()));
+      const coincideCategoria = categoria === 'todos' || (producto.categoria && producto.categoria === categoria);
+
+      const precio = parseFloat(producto.precio);
+      const coincidePrecio = (precioMinimo === 0 && precioMaximo === 0) ||
+                              (precio >= precioMinimo && precio <= precioMaximo);
+      const coincideCalificacion = (calificacion === 0 || producto.rating === calificacion);
+
+      return coincideBusqueda && coincideCategoria && coincidePrecio && coincideCalificacion;
+    });
+
+    setProductosFiltrados(productosFiltrados);
+  };
+
+  const agregarAlCarrito = (producto) => {
+    const carrito = JSON.parse(localStorage.getItem('carrito')) || [];
+    const productoExistente = carrito.find(item => item.idProducto === producto.idProducto);
+
+    if (productoExistente) {
+      productoExistente.cantidad += 1;
+    } else {
+      carrito.push({ ...producto, cantidad: 1 });
+    }
+
+    localStorage.setItem('carrito', JSON.stringify(carrito));
+    alert('Producto agregado al carrito');
+  };
+
+  const alternarFiltro = () => {
+    setFiltroAbierto(!filtroAbierto);
+  };
+
+  const manejarCambioBusqueda = (e) => {
+    setTerminoBusqueda(e.target.value);
+  };
+
+  const manejarCambioCategoria = (e) => {
+    setCategoria(e.target.value);
+  };
+
+  const manejarCambioPrecio = () => {
+    filtrarProductos(terminoBusqueda, categoria, [precioMinimo, precioMaximo], calificacion);
+  };
+
+  const manejarCambioCalificacion = (calificacion) => {
+    setCalificacion(calificacion);
+  };
+
+  const formatearPrecio = (precio) => {
+    return new Intl.NumberFormat('es-CO', { style: 'currency', currency: 'COP' }).format(precio);
+  };
+
+  const resetearPrecios = () => {
+    setPrecioMinimo(0);
+    setPrecioMaximo(0);
+    filtrarProductos(terminoBusqueda, categoria, [0, 0], calificacion);
+  };
+
+  const abrirModal = (producto) => {
+    setProductoSeleccionado(producto);
+    setModalAbierto(true);
+  };
+
+  const cerrarModal = () => {
+    setModalAbierto(false);
+    setProductoSeleccionado(null);
+  };
+
+  return (
+    <div className="flex flex-col min-h-screen bg-gray-200">
+      <Header />
+      <div className="flex flex-col md:flex-row flex-1">
+        <div className={`md:w-1/4 lg:w-1/5 bg-white border rounded-lg overflow-hidden shadow-md p-4 ${filtroAbierto ? 'block' : 'hidden md:block'}`}>
+          <div className="flex justify-between items-center mb-4">
+            <h2 className="text-lg font-bold">Bienvenido</h2>
+            <button onClick={alternarFiltro} className="text-darkyellow text-xl">
+              {/* Add icon here if needed */}
+            </button>
+          </div>
+          <div>
+            <div className="flex items-center mb-4">
+              <label htmlFor="search" className="block text-sm font-bold mb-2"></label>
+              <div className="relative flex-1">
+                <input
+                  type="text"
+                  id="search"
+                  value={terminoBusqueda}
+                  onChange={manejarCambioBusqueda}
+                  className="shadow border rounded w-full py-2 px-3 pr-12"
+                />
+                <div className="absolute inset-y-0 right-0 flex items-center px-2">
+                  <FaSearch className="text-darkpurple" />
+                </div>
+              </div>
             </div>
-            <Footer />
+            <label htmlFor="category" className="block text-sm font-bold mb-2">Categoría:</label>
+            <select
+              id="category"
+              value={categoria}
+              onChange={manejarCambioCategoria}
+              className="shadow border rounded w-full py-2 px-3 mb-4"
+            >
+              <option value="todos">Todos</option>
+              {opcionesCategorias.map(categoria => (
+                <option key={categoria.value} value={categoria.value}>
+                  {categoria.label}
+                </option>
+              ))}
+            </select>
+            <label className="block text-sm font-bold mb-2">Rango de precio:</label>
+            <div className="flex mb-4">
+              <input
+                type="number"
+                id="minPrice"
+                value={precioMinimo}
+                onChange={e => setPrecioMinimo(parseFloat(e.target.value))}
+                className="shadow border rounded w-1/2 py-2 px-3 mr-2"
+                placeholder="Mínimo"
+              />
+              <input
+                type="number"
+                id="maxPrice"
+                value={precioMaximo}
+                onChange={e => setPrecioMaximo(parseFloat(e.target.value))}
+                className="shadow border rounded w-1/2 py-2 px-3"
+                placeholder="Máximo"
+              />
+            </div>           
+            
+            <button onClick={resetearPrecios} className="text-darkyellow">Resetear precios</button>
+          </div>
         </div>
-    );
-};
+  
+        {/* Productos */}
+        <div className="flex-1 p-4">
+          <h2 className="text-2xl font-bold mb-4">Artesanías</h2>
+          {cargando ? (
+            <div className="text-center">Cargando productos...</div>
+          ) : error ? (
+            <div className="text-red-500 text-center">{error}</div>
+          ) : productosFiltrados.length === 0 ? (
+            <div className="text-center">No se encontraron productos.</div>
+          ) : (
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+              {productosFiltrados.map(producto => (
+                <div
+                  key={producto.idProducto}
+                  className="bg-white p-4 rounded-lg shadow-md cursor-pointer"
+                  onClick={() => abrirModal(producto)}
+                >
+                   <img
+                    src={producto.urlProductoImg ? `https://imagenes224.blob.core.windows.net/imagenes224/${producto.urlProductoImg.split('/').pop()}` : imgPrueba}
+                    alt={producto.nombre}
+                    className="w-full h-48 object-cover mb-4 rounded"
+                  />
+                  <h3 className="text-2xl font-semibold mt-5">{producto.nombreProducto}</h3>
+                  <p className="text-gray-600 mt-3 mb-3">{formatearPrecio(producto.precio)}</p>
+                  <p className="text-gray-800 mb-3"> {producto.descripcion}</p>
+                  <button
+  onClick={() => agregarAlCarrito(productoSeleccionado)}
+  className="bg-darkyellow hover:bg-lightyellow text-white py-2 px-4 rounded mt-3"
+>
+  Agregar al carrito
+</button>
+                </div>
+              ))}
+            </div>
+          )}
+        </div>
+      </div>
+  
+      {/* Modal de Producto */}
+      {modalAbierto && productoSeleccionado && (
+        <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
+          <div className="bg-white p-6 rounded-lg shadow-lg w-3/4 md:w-1/2 lg:w-1/3 relative">
+          <div className='flex justify-center'>
+      <h3 className="text-xl font-bold mb-4 ">{productoSeleccionado.nombreProducto}</h3>
+      </div>
+            <img
+        src={
+          productoSeleccionado.urlProductoImg
+            ? `https://imagenes224.blob.core.windows.net/imagenes224/${productoSeleccionado.urlProductoImg.split('/').pop()}`
+            : imgPrueba
+        }
+        alt={productoSeleccionado.nombre}
+        className="w-full h-64 object-contain mb-4 rounded"
+      />
+      <p className="text-gray-600 mb-2 "> <span className='font-bold'>Precio:</span> {formatearPrecio(productoSeleccionado.precio)}</p>
+      <p className="text-gray-800 mb-2"> <span className='font-bold'>Descripción:</span> {productoSeleccionado.descripcion}</p>
+      <p className="text-gray-600 mb-2"> <span className='font-bold'>Materiales:</span> {productoSeleccionado.materiales}</p>
+      <p className="text-gray-600 mb-2"> <span className='font-bold'>Cantidad disponible:</span> {productoSeleccionado.cantidad}</p>
+      <p className="text-gray-600 mb-2"> <span className='font-bold'>Publicado por:</span> {productoSeleccionado.publicadoPor}</p>
+            <div className="flex justify-between mt-4">
+            <button
+  onClick={() => agregarAlCarrito(productoSeleccionado)}
+  className="bg-darkyellow hover:bg-lightyellow text-white py-2 px-4 rounded mt-3"
+>
+  Agregar al carrito
+</button>
+        <button
+          onClick={cerrarModal}
+          className="bg-gray-300 text-gray-800 py-2 px-4 rounded"
+        >
+          Cerrar
+        </button>
+            </div>
+          </div>
+        </div>
+      )}
+  
+      <Footer />
+    </div>
+  );
+}  
+
+
+
+
+
